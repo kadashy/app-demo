cac1fb3c6934f0c9989f61b5411993bddcffbeba
Andres Restrepo <aerestrepoa@sodimac.cl>
2020-06-25 09:50:47 -0400

    Add app

---
+++ manage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""App demo for run in k8s"""
+
+__version__ = "0.0.1"
+__author__ = "Andres Restrepo <andres.e.restrepo.a@gmail.com>"
+
+from flask_restful import reqparse, abort, Api, Resource
+from flask import Flask
+import os
+import logging
+
+app = Flask(__name__)
+api = Api(app)
+
+env_1 = os.getenv('ENV_1')
+env_2 = os.getenv('ENV_2')
+
+class Healthy(Resource):
+    def get(self):
+        status = {'status': 'up', 'Env 1':env_1}, 200
+        logging.debug(status, extra={'site': 'SOCL'})
+        return status
+
+class UnHealthy(Resource):
+    def get(self):
+        status = {'status': 'down'}, 500
+        logging.debug(status, extra={'site': 'SOCL'})
+        return status
+
+class RunTimeError(Resource):
+    def get(self):
+        os._exit(0)
+
+api.add_resource(Healthy, "/probe/healthy")
+api.add_resource(UnHealthy, "/probe/unhealthy")
+api.add_resource(RunTimeError, "/probe/error")
+
+if __name__ == '__main__':
+    logging.info("App demo started")
+    app.run()
